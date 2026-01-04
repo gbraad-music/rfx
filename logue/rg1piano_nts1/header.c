7be99d2bac51fbaf3e7ac92fd4bc889f03046846
Gerard Braad <me@gbraad.nl>
2026-01-04 20:09:23 +0800

    Add a low frequency oscillation

--- logue/rg1piano_nts1/header.c
+++ logue/rg1piano_nts1/header.c
@@ -13,7 +13,7 @@ const __unit_header unit_header_t unit_header = {
     .unit_id = 0x2U,
     .version = 0x00010000U,
     .name = "RG1Piano",
-    .num_params = 4,
+    .num_params = 6,
     .params = {
         // Shape knob - Decay time
         {0, 1023, 0, 512, k_unit_param_type_none, 0, 0, 0, {"DECAY"}},
@@ -24,8 +24,8 @@ const __unit_header unit_header_t unit_header = {
         // Edit menu parameters
         {0, 1023, 0, 800, k_unit_param_type_none, 0, 0, 0, {"VEL SENS"}},
         {0, 1023, 0, 850, k_unit_param_type_none, 0, 0, 0, {"VOLUME"}},
-        {0, 0, 0, 0, k_unit_param_type_none, 0, 0, 0, {""}},
-        {0, 0, 0, 0, k_unit_param_type_none, 0, 0, 0, {""}},
+        {0, 1023, 0, 300, k_unit_param_type_none, 0, 0, 0, {"LFO RATE"}},
+        {0, 1023, 0, 200, k_unit_param_type_none, 0, 0, 0, {"LFO DPTH"}},
         {0, 0, 0, 0, k_unit_param_type_none, 0, 0, 0, {""}},
         {0, 0, 0, 0, k_unit_param_type_none, 0, 0, 0, {""}},
         {0, 0, 0, 0, k_unit_param_type_none, 0, 0, 0, {""}},

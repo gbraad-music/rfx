f3aafa518db0435645e6c3d6fa551bfe5d25ffe4
Gerard Braad <me@gbraad.nl>
2026-01-11 23:48:42 +0800

    Correctly compile AHX test tool after refactor

--- tools/ahxplayer/build_ahx_player.sh
+++ tools/ahxplayer/build_ahx_player.sh
@@ -2,7 +2,12 @@
 # Build AHX player test tool
 set -e
 echo "Building AHX player test tool..."
-gcc -o ahx_player_test ahx_player_test.c ../../synth/ahx_player.c -I../../synth -lSDL2 -lm -O2 -Wall
+gcc -o ahx_player_test ahx_player_test.c \
+    ../../synth/ahx_player.c \
+    ../../synth/tracker_modulator.c \
+    ../../synth/tracker_sequence.c \
+    ../../synth/tracker_voice.c \
+    -I../../synth -lSDL2 -lm -O2 -Wall
 echo "Build complete: ahx_player_test"
 echo ""
 echo "Usage: ./ahx_player_test <file.ahx> [-o output.wav] [-c channels] [-s subsong]"
